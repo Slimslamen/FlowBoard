741ce1c960679f8ea054f60b608855be6f50c923
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-10-14 14:16:01 +0200

    Mapped tasks

--- frontend/src/app/Components/card/card.component.ts
+++ frontend/src/app/Components/card/card.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ITasks } from './TasksModel';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
+
 export class CardComponent {
 
+TasksArray: ITasks[][] = [
+  [
+    { id: 1, task: "Clean" },
+    { id: 2, task: "Shower" }
+  ],
+  [
+    { id: 1, task: "Eat" },
+    { id: 2, task: "Sleep" }
+  ],
+  [
+    { id: 1, task: "Train" },
+    { id: 2, task: "TV" }
+  ]
+];
 }
