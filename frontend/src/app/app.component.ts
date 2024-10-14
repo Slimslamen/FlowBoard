5bd069054271e578ae5b91e8bc6cb74d7f7e54f7
jimmy.lopezmorales <jimmy.lopezmorales@utb.ecutbildning.se>
2024-10-14 10:46:02 +0200

    Components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { LoginComponent } from "./Components/login/login.component";
+import { BoardComponent } from "./Components/board/board.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, BoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
