a8e7d224299055a3580565ec8ce0403c8e8392ec
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-10-15 09:02:11 +0200

    Hero page and Router Links are fixed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,12 +5,13 @@ import { BoardComponent } from "./Components/board/board.component";
 import { AdminComponent } from "./Components/login/admin/admin.component";
 import { UserComponent } from "./Components/login/user/user.component";
 import { RouterModule } from '@angular/router';
+import { HeroPageComponent } from './Components/hero-page/hero-page.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, BoardComponent, AdminComponent, UserComponent],
+  imports: [RouterOutlet, NavbarComponent, BoardComponent, AdminComponent, UserComponent, HeroPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
