b577f5f390ef9f8eddd594fb3cefd92972e527e7
estelle_s <estelle.stenemur@utb.ecutbildning.se>
2024-11-11 09:25:58 +0100

    delete user works

--- frontend/src/app/Services/Admin/admin-service.service.ts
+++ frontend/src/app/Services/Admin/admin-service.service.ts
@@ -18,8 +18,7 @@ export class AdminServiceService {
     return this.http.get<IBoards[]>("http://localhost:5228/api/users/GetUserBoard?id="+id, {withCredentials:true})
   }
   DeleteUserForAdmin(userId:string):Observable<IAccessUser>{
-    const headers={'content-type': 'application/json',
-      'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE'
+    const headers={'content-type': 'application/json'
     }
     return this.http.delete<IAccessUser>("http://localhost:5228/api/users/DeleteUser?userId="+ userId,{'headers':headers, withCredentials: true})
   }
